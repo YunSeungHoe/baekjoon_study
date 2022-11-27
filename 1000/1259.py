12aefb28b74f54028abdfe4050bf94c6390fdd98
YunSeungHoe <dbstmdghl@naver.com>
2022-11-27 21:09:04 +0900

    update solved 264

---
+++ 1000/1259.py
@@ -0,0 +1,5 @@
+n = input()
+while n != '0':
+    if n == n[::-1]: print("yes")
+    else: print("no")
+    n = input()
--- 1260.py
+++
@@ -1,7 +0,0 @@
-node, edge, start = map(int, input().split())
-graph = {}
-for i in range(edge):
-    n, m = map(int, input().split())
-    graph[n] = [graph[n], m]
-    graph[m] = [graph[m], n]
-print(graph)
